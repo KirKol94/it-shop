83d0118a9a04f87adc476941486002b17a9850d7
Kirill Kolchanov <kirkol94@ya.ru>
2023-03-02 00:56:13 +0500

    Настроил тип модалки и добавил возможность закрыть её по нажатию на escape

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,8 +1,10 @@
-import React, { FC, ReactNode } from "react";
+import React, { FC, ReactNode, useEffect } from "react";
 import ReactDOM from "react-dom";
 import styles from "./Modal.module.scss";
 
-const portal: any = document.getElementById("modal");
+const portal: Element | null = document.querySelector(
+  "#modal"
+) as HTMLDivElement;
 
 interface IProps {
   isOpen: boolean;
@@ -11,24 +13,37 @@ interface IProps {
 }
 
 const Modal: FC<IProps> = ({ isOpen, setIsOpen, children }) => {
+  // закрываем модалку при клике на полупрозрачный оверлей
   const onClickOverlay = (e: React.MouseEvent<HTMLDivElement>) => {
     setIsOpen(false);
   };
 
+  // пропускаем закрытие модалки по клику по ней
   const onClickModal = (e: React.MouseEvent<HTMLDivElement>) => {
     e.stopPropagation();
   };
 
+  // по нажатию на escape закрываем модалку
+  const handleKeyDown = (e: any) => {
+    if (isOpen && e.key === "Escape") setIsOpen(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [isOpen]);
+
+  // ничего не показываем, если проп isOpen === false
+  if (!isOpen) return null;
+
   return ReactDOM.createPortal(
-    <>
-      {isOpen && (
-        <div className={styles.overlay} onClick={onClickOverlay}>
-          <div className={styles.modal} onClick={onClickModal}>
-            {children}
-          </div>
-        </div>
-      )}
-    </>,
+    <div className={styles.overlay} onClick={onClickOverlay}>
+      <div className={styles.modal} onClick={onClickModal}>
+        {children}
+      </div>
+    </div>,
     portal
   );
 };
