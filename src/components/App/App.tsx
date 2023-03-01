4727492dceba9cea2f183c2fbb1c1429d9ea5206
Kirill Kolchanov <kirkol94@ya.ru>
2023-03-01 19:08:00 +0500

    добавил layout outlet для роутинга (в outlet меняется контент)

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,15 +1,18 @@
-import React, { FC } from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from '../../pages/Home/Home'
-import Page404 from '../../pages/Page404/Page404'
+import React, { FC } from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "../../pages/Home/Home";
+import Page404 from "../../pages/Page404/Page404";
+import Layout from "../Layout/Layout";
 
 const App: FC = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="*" element={<Page404/>}/>
+      <Route path="" element={<Layout />}>
+        <Route path="" element={<Home />} />
+        <Route path="*" element={<Page404 />} />
+      </Route>
     </Routes>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,22 @@
+import React, { FC } from "react";
+import { Link } from "react-router-dom";
+import styles from "./Header.module.scss";
+
+const Header: FC = () => {
+  return (
+    <header>
+      <nav className={styles.nav}>
+        <ul className={styles.list}>
+          <li>
+            <Link to="/">Главная</Link>
+          </li>
+          <li>
+            <Link to="/not-found">Не существующая</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
