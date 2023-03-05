5f67c5524503d06debf12b1eadf244c0d1ef8c16
Kirill Kolchanov <kirkol94@ya.ru>
2023-03-05 17:17:01 +0500

    kirkol | пофиксил бордер-радиус у второго варианта кнопки

--- src/pages/TestPage/TestPage.tsx
+++ src/pages/TestPage/TestPage.tsx
@@ -4,6 +4,7 @@ import { fetchProducts } from "../../store/products/asyncProductActions";
 import { fetchCategories } from "../../store/categories/asyncCategoryActions";
 import Card from "../../components/ui/Card/Card";
 import styles from "./TestPage.module.scss";
+import Button from "../../components/ui/Button/Button";
 
 const TestPage = () => {
   const dispatch = useAppDispatch();
@@ -21,22 +22,49 @@ const TestPage = () => {
   }
 
   return (
-    <div className={styles.testPage}>
-      <div className={styles.testPage__container}>
-        <h2 className={styles.title}>Футболки и свитшоты</h2>
+    <>
+      <section className={styles.heroSection}>
+        <div className={styles.heroSection__bg} />
 
-        <div className={styles.cards}>
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+        <div className={styles.heroSection__container}>
+          <h1 className={styles.heroSection__title}>
+            Первый мерч-шоп осознанной меркантильности
+          </h1>
+
+          <p className={styles.heroSection__subtitle}>
+            Первый мерч-шоп осознанной меркантильности (h1) Создано отчаявшимися
+            найти валютную удалёнку джунами для таких же джунов В каталог мерча
+            Нанять талантливых джунов
+          </p>
+
+          <div className={styles.heroSection__actions}>
+            <Button variant="main" className="button" onClick={() => {}}>
+              В каталог мерча
+            </Button>
+            <Button variant="second" className="button" onClick={() => {}}>
+              Нанять талантливых джунов
+            </Button>
+          </div>
         </div>
-      </div>
-    </div>
+      </section>
+
+      <section className={styles.cards}>
+        <div className={styles.cards__container}>
+          <h2 className={styles.cards__title}>Футболки и свитшоты</h2>
+
+          <div className={styles.cards__items}>
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
