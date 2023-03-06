9a98e7d20a241a4cc288078ccb15130777eb0463
Kirill Kolchanov <kirkol94@ya.ru>
2023-03-06 09:42:30 +0500

    kirkol | прописал роутинги, по которым открываются модальные окна авторизации и регистрации

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,16 @@ import Home from "./pages/Home/Home";
 import Page404 from "./pages/Page404/Page404";
 import PageStructure from "./components/layout/PageStructure/PageStructure";
 import TestPage from "./pages/TestPage/TestPage";
+import AuthModal from './components/modals/AuthModal/AuthModal'
+import RegisterModal from './components/modals/RegisterModal/RegisterModal'
 
 const App: FC = () => {
   return (
     <Routes>
       <Route path="" element={<PageStructure />}>
         <Route path="" element={<Home />} />
+        <Route path="login" element={<AuthModal />} />
+        <Route path="signup" element={<RegisterModal />} />
         <Route path="test-page" element={<TestPage />} />
         <Route path="*" element={<Page404 />} />
       </Route>
