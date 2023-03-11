c64575fd14db114e6dd29220ad2ad8a4a26f3092
kirill kolchanov <kirkol94@ya.ru>
2023-03-11 18:40:42 +0500

    kirkol | изменил папку Header, HeaderCategories, HeaerTop на нижний регистр

--- src/components/layout/PageStructure/PageStructure.tsx
+++ src/components/layout/PageStructure/PageStructure.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from "react";
 import { Outlet } from "react-router-dom";
-import Header from "../Header/Header";
+import Header from "../header/Header";
 import Footer from "../footer/Footer";
 
 const PageStructure: FC = () => (
