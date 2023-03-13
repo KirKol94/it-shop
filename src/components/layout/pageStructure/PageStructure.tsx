881394bd9bacf321eb77ca945427584e367f1509
kirill kolchanov <kirkol94@ya.ru>
2023-03-13 12:32:13 +0500

    kirkol | зафиксировал HeaderCategories block

--- src/components/layout/pageStructure/PageStructure.tsx
+++ src/components/layout/pageStructure/PageStructure.tsx
@@ -2,16 +2,23 @@ import React, { FC } from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from '../header/Header'
 import Footer from '../footer/Footer'
+import HeaderCategories from '../header/headerCategories/HeaderCategories'
+import styled from 'styled-components'
+
+const StyledPageStructure = styled.div`
+  position: relative;
+`
 
 const PageStructure: FC = () => (
-  <>
+  <StyledPageStructure>
     <Header />
+    <HeaderCategories />
 
     {/* тут будет меняться контент в зависимости от роута */}
     <Outlet />
 
     <Footer />
-  </>
+  </StyledPageStructure>
 )
 
 export default PageStructure
