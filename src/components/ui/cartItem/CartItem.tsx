bbc36536ab591f749472b523d45d5fd72b1d94ef
amigo <suleymandzyazikov@gmail.com>
2023-03-14 21:49:20 +0300

    AMIGO | начал писать карточку товара для корзины

--- src/components/ui/cartItem/CartItem.tsx
+++ src/components/ui/cartItem/CartItem.tsx
@@ -1,8 +1,61 @@
 import React from 'react'
-import { Wrapper } from './styled'
+import CloseIcon from '@ui/icons/CloseIcon'
+
+import {
+  Wrapper,
+  Img,
+  ImgBox,
+  Inner,
+  Header,
+  Price,
+  Title,
+  Sizes,
+  SizeName,
+  Footer,
+} from './styled'
 
 const CartItem = () => {
-  return <Wrapper>asdasdf</Wrapper>
+  const sizes = ['l', 'xl']
+
+  return (
+    <Wrapper>
+      <ImgBox>
+        <Img />
+      </ImgBox>
+      <Inner>
+        <Header>
+          <Title></Title>
+          <CloseIcon size={20} />
+        </Header>
+
+        <Sizes>
+          <p>Размер:</p>
+          {sizes.map(size => {
+            ;<SizeName key={size}>{size}</SizeName>
+          })}
+        </Sizes>
+
+        <Footer>
+          <CountBox>
+            <CountBtn action="minus" onClick={decrement}>
+              -
+            </CountBtn>
+            <CountInput
+              type="text"
+              inputMode="numeric"
+              value={count}
+              onChange={handleInputChange}
+              maxLength={3}
+            />
+            <CountBtn action="plus" onClick={increment}>
+              +
+            </CountBtn>
+          </CountBox>
+          <Price>₽</Price>
+        </Footer>
+      </Inner>
+    </Wrapper>
+  )
 }
 
 export default CartItem
