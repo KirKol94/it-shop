acf2e2401decaa498b33f0473189ff78a6571e51
kirill kolchanov <kirkol94@ya.ru>
2023-03-14 10:19:49 +0500

    kirkol | подготовил компонент карточки для корзины

---
+++ src/components/ui/cartItem/styled.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+import { bgColors } from '@/styled/vars'
+
+export const Wrapper = styled.div`
+  background-color: ${bgColors.darkBlock};
+  border-radius: 16px;
+  padding: 16px;
+`
