086ae887981dc956770ff07bdd1fdbf04a4a5795
kirill kolchanov <kirkol94@ya.ru>
2023-03-15 13:19:42 +0500

    kirkol | удалил страницу test-pag, добавил policy-privacy

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,14 @@ import { Route, Routes } from 'react-router-dom'
 import Home from '@/pages/Home/Home'
 import Page404 from '@/pages/Page404/Page404'
 import PageStructure from '@/components/layout/pageStructure/PageStructure'
-import TestPage from '@/pages/TestPage/TestPage'
+import PrivacyPolicy from '@/pages/privacyPolicy/PrivacyPolicy'
 
 const App: FC = () => {
   return (
     <Routes>
       <Route path="" element={<PageStructure />}>
         <Route path="" element={<Home />} />
-        <Route path="test-page" element={<TestPage />} />
+        <Route path="privacy-policy" element={<PrivacyPolicy />} />
         <Route path="*" element={<Page404 />} />
       </Route>
     </Routes>
