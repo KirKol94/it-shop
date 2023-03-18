d89be67bb679faa8f39acef246ee1ada6fc675f8
kirill kolchanov <kirkol94@ya.ru>
2023-03-18 14:35:28 +0500

    kirkol | адаптив Card

--- src/components/ui/card/styled.ts
+++ src/components/ui/card/styled.ts
@@ -6,7 +6,7 @@ import { RootButton } from '@/styled/root'
 export const Wrapper = styled.div`
   display: flex;
   width: 100%;
-  max-width: 280px;
+  max-width: clamp(11.25rem, 7.885rem + 12.82vw, 17.5rem);
   flex-direction: column;
   gap: 12px;
   padding: 20px;
@@ -26,7 +26,7 @@ export const Wrapper = styled.div`
 
 export const ImgBox = styled.div`
   display: flex;
-  height: 200px;
+  height: clamp(8rem, 5.745rem + 8.59vw, 12.188rem);
   align-items: center;
   justify-content: center;
   border-radius: 16px;
@@ -43,22 +43,23 @@ export const Img = styled.img`
 export const Body = styled.div`
   flex: 1 1 auto;
   text-align: center;
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
 `
 
 export const Subtitle = styled.p`
-  margin-bottom: 12px;
   color: ${textColors.gray};
   font-weight: 300;
-  font-size: 14px;
+  font-size: clamp(0.688rem, 0.587rem + 0.38vw, 0.875rem);
   line-height: 1.5;
   ${cutLongStringStyles(2)}
 `
 
 export const Title = styled.h3`
-  margin-bottom: 12px;
   color: ${textColors.main};
   font-weight: 600;
-  font-size: 24px;
+  font-size: clamp(1rem, 0.731rem + 1.03vw, 1.5rem);
   line-height: 1.25;
   ${cutLongStringStyles(2)}
 `
@@ -69,6 +70,7 @@ export const Footer = styled.div`
   flex-direction: column;
   gap: 12px;
 `
+
 export const FooterPrices = styled.div`
   display: flex;
   gap: 12px;
@@ -78,15 +80,16 @@ export const FooterPrices = styled.div`
 
 export const Price = styled.span`
   color: ${textColors.main};
-  font-size: 20px;
+  font-size: clamp(0.875rem, 0.673rem + 0.77vw, 1.25rem);
   font-weight: 600;
   line-height: 1;
 `
+
 export const OldPrice = styled.span`
   position: relative;
   color: ${textColors.gray};
   font-weight: 600;
-  font-size: 14px;
+  font-size: clamp(0.875rem, 0.875rem + 0vw, 0.875rem);
   line-height: 1.25;
 
   &::before {
@@ -103,4 +106,8 @@ export const OldPrice = styled.span`
 
 export const Button = styled(RootButton)`
   color: ${textColors.main};
+`
+
+export const CardButton = styled(RootButton)`
+  padding: clamp(0.281rem, 0.096rem + 0.71vw, 0.625rem) 0;
 `
