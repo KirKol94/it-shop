a5d00da5e61bfc3661cfe8910fcdafac150e13d7
kirill kolchanov <kirkol94@ya.ru>
2023-03-21 21:26:11 +0500

    kirkol | пофиксил отступы между элементами навигации

--- src/components/ui/navigation/styled.ts
+++ src/components/ui/navigation/styled.ts
@@ -5,16 +5,16 @@ export const NavList = styled.ul<{ isVertical?: boolean }>`
   display: flex;
   flex-direction: ${({ isVertical }) => (isVertical ? 'column' : 'row')};
   align-items: center;
-  gap: 32px;
+  gap: clamp(1rem, -3.797rem + 7.73vw, 2rem);
 `
 
 export const NavItem = styled.li`
   font-weight: 600;
   transition: all 150ms;
+  text-align: center;
 
   ${bp.lg} {
     width: 100%;
-    text-align: center;
   }
 
   &:hover {
