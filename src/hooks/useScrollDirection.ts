b2502934e48a764450cb68c2c71790a38e7aac93
kirill kolchanov <kirkol94@ya.ru>
2023-03-22 23:50:19 +0500

    kirkol | поправил хук определения движения скрола

--- src/hooks/useScrollDirection.ts
+++ src/hooks/useScrollDirection.ts
@@ -11,7 +11,7 @@ const useScrollDirection = () => {
       const direction = scrollY > lastScrollY ? 'down' : 'up'
       if (
         direction !== scrollDirection &&
-        (scrollY - lastScrollY > 5 || scrollY - lastScrollY < -5)
+        (scrollY - lastScrollY > 1 || scrollY - lastScrollY < -1)
       ) {
         setScrollDirection(direction)
       }
