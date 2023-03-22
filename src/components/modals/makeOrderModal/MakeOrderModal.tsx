9e2b1d4b1e62da08b94d8e9338fcb8467f1e43e4
kirill kolchanov <kirkol94@ya.ru>
2023-03-22 15:43:44 +0500

    kirkol | Модалка оформления заказа

--- src/components/modals/makeOrderModal/MakeOrderModal.tsx
+++ src/components/modals/makeOrderModal/MakeOrderModal.tsx
@@ -1,8 +1,19 @@
 import React, { FC } from 'react'
 import Menu from '@ui/menu/Menu'
-import { Wrapper } from './styled'
+import {
+  Button,
+  CloseBox,
+  Form,
+  FormTitle,
+  Header,
+  Input,
+  InputRow,
+  Title,
+  Wrapper,
+} from './styled'
 import { useAppDispatch, useAppSelector } from '@/hooks/reduxHooks'
 import { setIsOpenMakeOrder } from '@/store/dialogWindows/dialogWindowsSlice'
+import { ReactComponent as CloseIcon } from '@/assets/svg/close.svg'
 
 const MakeOrderModal: FC = () => {
   const dispatch = useAppDispatch()
@@ -13,7 +24,43 @@ const MakeOrderModal: FC = () => {
 
   return (
     <Menu isOpen={isOpen} setIsOpen={setIsOpen}>
-      <Wrapper></Wrapper>
+      <Wrapper>
+        <Header>
+          <Title>Оформление заказа</Title>
+          <CloseBox>
+            <button onClick={() => setIsOpen(false)}>
+              <CloseIcon />
+            </button>
+          </CloseBox>
+        </Header>
+
+        <Form>
+          <FormTitle>Информация о покупателе</FormTitle>
+          <InputRow>
+            <Input placeholder="Имя" />
+            <Input placeholder="Фамилия" />
+          </InputRow>
+          <InputRow>
+            <Input placeholder="Телефон" />
+            <Input placeholder="Эл. почта" />
+          </InputRow>
+
+          <FormTitle>Адрес доставки</FormTitle>
+          <InputRow>
+            <Input placeholder="Страна" />
+            <Input placeholder="Город" />
+          </InputRow>
+          <Input placeholder="Адрес строка 1" />
+          <Input placeholder="Адрес строка 2" />
+
+          <FormTitle>Информация об оплате</FormTitle>
+          <Input placeholder="Извините, но я пока в душе не ебу, что сюда лепить" />
+
+          <Button onClick={() => setIsOpen(false)} variant="solid">
+            Перейти к оплате
+          </Button>
+        </Form>
+      </Wrapper>
     </Menu>
   )
 }
--- src/components/modals/makeOrderModal/styled.ts
+++ src/components/modals/makeOrderModal/styled.ts
@@ -1,14 +1,77 @@
 import styled from 'styled-components'
-import { colors } from '@/styled/vars'
+import { bp, colors, fonts } from '@/styled/vars'
+import { RootButton } from '@root/RootButton'
 
 export const Wrapper = styled.div`
+  overflow-y: auto;
   height: 100%;
   border-radius: 16px;
-  width: clamp(18.75rem, 12.841rem + 29.55vw, 35rem);
+  width: clamp(20rem, 10.227rem + 48.86vw, 46.875rem);
   padding: 20px;
   background-color: ${colors.violet};
   display: flex;
   gap: 10px;
   flex-direction: column;
-  justify-content: space-between;
+`
+
+export const Header = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+`
+
+export const Title = styled.h2`
+  font-weight: 500;
+  font-size: clamp(1.25rem, 0.795rem + 2.27vw, 2.5rem);
+  line-height: 125%;
+  padding-bottom: 10px;
+  border-bottom: 1px solid ${colors.neon};
+`
+
+export const CloseBox = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: end;
+`
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  row-gap: 16px;
+  column-gap: 20px;
+`
+
+export const FormTitle = styled.h3`
+  width: 100%;
+  font-weight: 500;
+  font-size: clamp(1rem, 0.455rem + 2.73vw, 2.5rem);
+  line-height: 125%;
+`
+
+export const InputRow = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  column-gap: 20px;
+  row-gap: 16px;
+
+  ${bp.lg} {
+    grid-template-columns: 1fr;
+  }
+`
+
+export const Input = styled.input`
+  color: ${colors.gray};
+  border: 1px solid ${colors.gray};
+  font-family: ${fonts.exo};
+  font-style: normal;
+  font-weight: 400;
+  font-size: clamp(0.875rem, 0.83rem + 0.23vw, 1rem);
+  line-height: clamp(1.063rem, 1.017rem + 0.23vw, 1.188rem);
+  background-color: transparent;
+  padding: clamp(0.688rem, 0.619rem + 0.34vw, 0.875rem) 22px;
+`
+
+export const Button = styled(RootButton)`
+  align-self: center;
+  width: 100%;
+  max-width: 380px;
 `
