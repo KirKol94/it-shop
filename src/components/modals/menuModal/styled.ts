4d040018570e0e0dd2f168858d95a8d041a3fef6
kirill kolchanov <kirkol94@ya.ru>
2023-03-23 23:37:05 +0500

    kirkol | пофиксил ширину менюшки для маленьких экранов

--- src/components/modals/menuModal/styled.ts
+++ src/components/modals/menuModal/styled.ts
@@ -1,14 +1,18 @@
 import styled from 'styled-components'
-import { colors } from '@/styled/vars'
+import { bp, colors } from '@/styled/vars'
 
 export const Wrapper = styled.div`
   height: 100%;
+  width: 460px;
   border-radius: 16px;
-  width: clamp(18.75rem, 12.841rem + 29.55vw, 35rem);
   padding: 20px;
   background-color: ${colors.violet};
   display: flex;
   gap: 10px;
   flex-direction: column;
   justify-content: space-between;
+
+  ${bp.sm} {
+    width: 100%;
+  }
 `
