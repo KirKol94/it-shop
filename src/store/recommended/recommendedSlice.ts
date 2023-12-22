e62985d36cff3067529b7a9cfb02f1a11ccdacc4
kirkol <kirkol94@ya.ru>
2023-12-22 20:21:32 +0500

    вынес массив продуктов в константу моковых данных

--- src/store/recommended/recommendedSlice.ts
+++ src/store/recommended/recommendedSlice.ts
@@ -1,6 +1,7 @@
 import { PayloadAction, createSlice } from '@reduxjs/toolkit'
 
 import { IProduct } from '@/types/IProduct'
+import { PRODUCTS } from '../mock/products'
 
 type recommendedState = {
   isLoading: boolean
@@ -9,53 +10,7 @@ type recommendedState = {
 
 const initialState: recommendedState = {
   isLoading: false,
-  products: [
-    {
-      id: '5',
-      isHasSizes: true,
-      category: { name: 'Футболка' },
-      image:
-        'https://cdn.discordapp.com/attachments/1080428490379247716/1086233923924791328/image.png',
-      name: 'IT-Пузырь',
-      description: 'Тряпка для мужиков с круглым авторитетом',
-      price: 1399,
-      oldPrice: 1599,
-    },
-    {
-      id: '6',
-      isHasSizes: true,
-      category: { name: 'Футболка' },
-      image:
-        'https://cdn.discordapp.com/attachments/1080428490379247716/1086235329457033246/image.png',
-      name: 'Меня мама любит',
-      description: 'Не, ну по-факту же',
-      price: 1299,
-      oldPrice: 1499,
-    },
-    {
-      id: '7',
-      isHasSizes: true,
-      category: { name: 'Футболка' },
-      image:
-        'https://kolchanov.notion.site/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2F62763b15-906b-40b3-862b-867be8f6bb28%2Fdfae2391-e30f-4744-9bfd-8fc7eed3a2d4%2F3925-t-shirt-black-shirt-image-png.png?table=block&id=72fc0c27-ac19-4504-b45c-577d5a6eb66d&spaceId=62763b15-906b-40b3-862b-867be8f6bb28&width=2000&userId=&cache=v2',
-      name: 'Видишь принт? Нет? А он есть',
-      description:
-        'Футболка с модным принтом с полностью прозрачным альфа каналом',
-      price: 5999,
-      oldPrice: 6599,
-    },
-    {
-      id: '8',
-      isHasSizes: true,
-      category: { name: 'Футболка' },
-      image:
-        'https://www.notion.so/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2F62763b15-906b-40b3-862b-867be8f6bb28%2F5766066a-0b6b-420e-bf94-5826380aa3ad%2Fi.png?table=block&id=bea780d3-8b8e-4bc1-8f9a-eb64e8b4392d&spaceId=62763b15-906b-40b3-862b-867be8f6bb28&width=2000&userId=2741abc2-75dd-4832-9063-4f0b928741d6&cache=v2',
-      name: 'Для тех, кто в теме',
-      description: 'Не обошлось без мемов',
-      price: 10999,
-      oldPrice: 11099,
-    },
-  ],
+  products: PRODUCTS,
 }
 
 const recommendedSlice = createSlice({
